3e0f9fe446f07eb1e2aedd84915e8f6dab650d1b
xom9ikk <xom9ik.code@gmail.com>
2020-07-19 17:45:13 +0300

    refactor(services): automatically convert snake <-> camel field names

--- src/knex/index.js
+++ src/knex/index.js
@@ -1,16 +1,19 @@
 const cluster = require('cluster');
 const knex = require('knex');
+const { Formatter } = require('../utils');
 
 const env = process.env.NODE_ENV;
 const dbConfig = require('../../knexfile')[env];
 
-class Database {
+class Knex {
   constructor() {
     this.knex = knex({
       ...dbConfig,
       pool: {
         ...dbConfig.pool,
       },
+      postProcessResponse: (result) => Formatter.deepConvertToCamelCase(result),
+      wrapIdentifier: (value, origImpl) => (value === '*' ? value : origImpl(Formatter.convertToSnakeCase(value))),
     });
     this.knex.migrate.latest();
     if (cluster.isMaster) {
