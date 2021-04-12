5308cb7505706b13192504d954c54b520ad41e81
Max Romanyuta <xom9ik.code@gmail.com>
2021-04-12 11:56:15 +0300

    fix: typo

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 echo 'env from sh'
 echo $NODE_ENV
 sleep 10
-npm run knex migrate:latest --env $NODE_ENV
+npx run knex migrate:latest --env $NODE_ENV
 pm2-runtime start /app/ecosystem.config.js --env $NODE_ENV
