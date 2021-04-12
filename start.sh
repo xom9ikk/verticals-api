b49a485ce780f5cadb7e3f3e6fd9e34d245cd219
Max Romanyuta <xom9ik.code@gmail.com>
2021-04-12 14:38:02 +0300

    refactor: remove extra scripts

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 echo 'env from sh'
 echo $NODE_ENV
 sleep 10
-npx run knex migrate:latest --env $NODE_ENV
+npx knex migrate:latest --env $NODE_ENV
 pm2-runtime start /app/ecosystem.config.js --env $NODE_ENV
